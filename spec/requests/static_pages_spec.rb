6a23b17ccc6e888d71883ba055f8faae21e373f1
Gregor <am@iworry.ru>
2013-12-29 07:31:00 +0400

    contact done

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,6 +15,19 @@ describe "Static pages" do
                         :text => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
+  describe "Contact page" do
+
+    it "should have the h1 'Sample App'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 
   describe "Help page" do
 
