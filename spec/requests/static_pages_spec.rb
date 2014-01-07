16b3b65262c719f590414f9aa385ab6bf367ea56
Gregor <am@iworry.ru>
2014-01-07 16:59:27 +0400

    --amend

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,26 +3,11 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-<<<<<<< HEAD
-    it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
-    end
-    it "should have the basetitle" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => 'Ruby on Rails')
-    end
-    it "should haven't the custom title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => '| Home')
-    end
-=======
   subject { page }
 
   shared_examples_for "all static pages" do
     it { should have_selector('h1',    text: "") }
     it { should have_selector('title', text: full_title(page_title)) }
->>>>>>> filling-in-layout
   end
   describe "Home page" do
     before { visit root_path }
