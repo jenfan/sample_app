9822eac4ccdc027c190bd6fec1a1e772bb975251
Gregor <am@iworry.ru>
2014-01-14 05:29:46 +0400

    Finish registration

--- spec/user_pages_spec.rb
+++ spec/user_pages_spec.rb
@@ -1,13 +1,8 @@
 require 'spec_helper'
 
-describe "UserPages" do
-subject { page }
+describe "User pages" do
 
-  describe "signup page" do
-  	before { visit signup_path }
-    it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
-  end
+  subject { page }
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
